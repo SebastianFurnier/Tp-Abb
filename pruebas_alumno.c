554420dbcc8f403742ca2f259786bec35eaa0eb2
sebastian <sfurnier@hotmail.com>
2023-04-30 18:00:49 -0300

    pasa todas las pruebas y esta formateado

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,17 +1,183 @@
 #include "pa2m.h"
+#include "./src/abb.h"
+#include "./src/abb_estructura_privada.h"
 
-void prueba_simple()
+int comparador_enteros(void *valor1, void *valor2)
 {
-	int i = 14;
-	int j = 16;
-	pa2m_afirmar(i != j, "i=14 es diferente de j=16");
+	return *(int*)(valor1) - *(int*)(valor2);
+}
+
+void sumar_uno(void *valor1)
+{
+	int *auxiliar = (int *)valor1;
+	(*auxiliar)++;
+	valor1 = (void *)auxiliar;
+}
+
+void pruebas_abb_a_array()
+{
+	abb_t *arbol = abb_crear(comparador_enteros);
+	if(!arbol)
+		return;
+	
+	int valor1 = 5;
+	int valor2 = 9;
+	int valor3 = 1;
+	int valor4 = 7;
+	size_t tamanio_arr = 4;
+
+	void *array[tamanio_arr];
+
+	abb_insertar(arbol, &valor1);
+	abb_insertar(arbol, &valor2);
+	abb_insertar(arbol, &valor3);
+	abb_insertar(arbol, &valor4);
+
+	abb_recorrer(arbol, INORDEN, array, tamanio_arr);
+	pa2m_afirmar(*(int*)array[0] == valor3, "Guardando los elementos INORDEN, el primer elemento de array es correcto.");
+	pa2m_afirmar(*(int*)array[1] == valor1, "Guardando los elementos INORDEN, el segundo elemento de array es correcto.");
+	pa2m_afirmar(*(int*)array[2] == valor4, "Guardando los elementos INORDEN, el tercero elemento de array es correcto.");
+	pa2m_afirmar(*(int*)array[3] == valor2, "Guardando los elementos INORDEN, el cuarto elemento de array es correcto.\n");
+	
+	abb_recorrer(arbol, PREORDEN, array, tamanio_arr);
+	pa2m_afirmar(*(int*)array[0] == valor1, "Guardando los elementos PREORDEN, el primer elemento de array es correcto.");
+	pa2m_afirmar(*(int*)array[1] == valor3, "Guardando los elementos PREORDEN, el segundo elemento de array es correcto.");
+	pa2m_afirmar(*(int*)array[2] == valor2, "Guardando los elementos PREORDEN, el tercero elemento de array es correcto.");
+	pa2m_afirmar(*(int*)array[3] == valor4, "Guardando los elementos PREORDEN, el cuarto elemento de array es correcto.\n");
+	
+	abb_recorrer(arbol, POSTORDEN, array, tamanio_arr);
+	pa2m_afirmar(*(int*)array[0] == valor3, "Guardando los elementos POSTORDEN, el primer elemento de array es correcto.");
+	pa2m_afirmar(*(int*)array[1] == valor4, "Guardando los elementos POSTORDEN, el segundo elemento de array es correcto.");
+	pa2m_afirmar(*(int*)array[2] == valor2, "Guardando los elementos POSTORDEN, el tercero elemento de array es correcto.");
+	pa2m_afirmar(*(int*)array[3] == valor1, "Guardando los elementos POSTORDEN, el cuarto elemento de array es correcto.");
+
+	abb_destruir(arbol);
+}
+
+void pruebas_destruir_todo()
+{
+	abb_t *arbol = abb_crear(comparador_enteros);
+	if(!arbol)
+		return;
+	
+	int valor1 = 5;
+	int valor2 = 9;
+	int valor3 = 6;
+	int valor4 = 7;
+
+	abb_insertar(arbol, &valor1);
+	abb_insertar(arbol, &valor2);
+	abb_insertar(arbol, &valor3);
+	abb_insertar(arbol, &valor4);
+
+	pa2m_afirmar(abb_tamanio(arbol) == 4, "Se insertan 4 elementos.");
+	abb_destruir_todo(arbol, sumar_uno);
+	
+	pa2m_afirmar(valor1 == 6 && valor2 == 10 && valor3 == 7 && valor4 == 8,
+			 "Se aplico correctamente la funcion enviada a destruir_todo a cada elemento del arbol.");
+
+}
+
+void pruebas_quitar_elementos()
+{
+	abb_t *arbol = abb_crear(comparador_enteros);
+	if(!arbol)
+		return;
+	
+	int valor1 = 5;
+	int valor2 = 9;
+	int valor3 = 6;
+	int valor4 = 7;
+	void *auxiliar;
+
+	abb_insertar(arbol, &valor1);
+	abb_insertar(arbol, &valor2);
+	abb_insertar(arbol, &valor3);
+	abb_insertar(arbol, &valor4);
+
+	auxiliar = abb_quitar(arbol, &valor3);
+	pa2m_afirmar(*(int*)auxiliar == valor3, "El elemento devuelto por la funcion quitar es el correcto.");
+	pa2m_afirmar(abb_tamanio(arbol) == 3, "Luego de quitar un elemento el tamanio es el correcto.");
+
+	auxiliar = abb_quitar(arbol, &valor3);
+	pa2m_afirmar(auxiliar == NULL, "Intentar quitar un elemento inexistente devuelve NULL.");
+	pa2m_afirmar(abb_tamanio(arbol) == 3, "Luego de intentar quitar un elemento inexistente el tamanio es el correcto.");
+
+	abb_quitar(arbol, &valor1);
+	abb_quitar(arbol, &valor2);
+	abb_quitar(arbol, &valor4);
+	pa2m_afirmar(abb_vacio(arbol) == true, "Luego de eliminar todos los elementos el arbol queda vacio.");
+	pa2m_afirmar(abb_tamanio(arbol) == 0, "El tamanio del arbol es el correcto.");
+
+	auxiliar = abb_quitar(arbol, NULL);
+	pa2m_afirmar(auxiliar == NULL, "Eliminar un elemento NULL, devuelve NULL.");
+	pa2m_afirmar(abb_tamanio(arbol) == 0, "Luego de intentar eliminar un elemento inexistente el tamanio es el correcto.");
+
+	auxiliar = abb_quitar(arbol, &valor1);
+	pa2m_afirmar(auxiliar == NULL, "Intentar quitar un elemento de un arbol vacio devuelve NULL.");
+
+	abb_destruir(arbol);
+}
+
+void pruebas_de_insercion()
+{
+	abb_t *arbol = abb_crear(comparador_enteros);
+	if(!arbol)
+		return;
+	
+	int cantidad_insertados = 0;
+	int valor1 = 5;
+	int valor2 = 9;
+	int valor3 = 6;
+	int valor4 = 7;
+
+	abb_insertar(arbol, &valor1);
+	pa2m_afirmar(abb_tamanio(arbol) == 1, "El arbol tiene el tamanio correcto luego de insertar un elemento.");
+	pa2m_afirmar(*(int*)abb_buscar(arbol, &valor1) == valor1, "El elemento se inserto correctamente.");
+	abb_insertar(arbol, &valor2);
+	abb_insertar(arbol, &valor3);
+	abb_insertar(arbol, &valor4);
+
+	if (abb_buscar(arbol, &valor1) != NULL)
+		cantidad_insertados++;
+	if (abb_buscar(arbol, &valor2) != NULL)
+		cantidad_insertados++;
+	if (abb_buscar(arbol, &valor3) != NULL)
+		cantidad_insertados++;
+	if (abb_buscar(arbol, &valor4) != NULL)
+		cantidad_insertados++;
+	pa2m_afirmar(cantidad_insertados == abb_tamanio(arbol), "Se buscan todos los elementos correctamente.");
+	pa2m_afirmar(abb_tamanio(arbol) == 4, "El arbol tiene el tamanio correcto luego de insertar varios elementos.");
+
+	abb_destruir(arbol);
+	return;
+}
+
+void pruebas_creacion_arbol()
+{
+	abb_t *arbol = abb_crear(NULL);
+	pa2m_afirmar(arbol == NULL, "No se puede crear un arbol con un comparador NULO");
+
+	arbol = abb_crear(comparador_enteros);
+	if(!arbol)
+		return;
+	pa2m_afirmar(arbol != NULL, "Se crea correctamente el arbol.");
+	pa2m_afirmar(abb_tamanio(arbol) == 0, "El tamanio de un arbol recien creado es 0.");
+
+	abb_destruir(arbol);
 }
 
 int main()
 {
-	pa2m_nuevo_grupo(
-		"\n======================== XXX ========================");
-	prueba_simple();
-
+	pa2m_nuevo_grupo("Prueba Creacion de arbol.");
+	pruebas_creacion_arbol();
+	pa2m_nuevo_grupo("Pruebas de insercion.");
+	pruebas_de_insercion();
+	pa2m_nuevo_grupo("Pruebas de quitar elementos.");
+	pruebas_quitar_elementos();
+	pa2m_nuevo_grupo("Pruebas Destruir todo.");
+	pruebas_destruir_todo();
+	pa2m_nuevo_grupo("Pruebas con Arrays");
+	pruebas_abb_a_array();
 	return pa2m_mostrar_reporte();
 }
--- src/abb.c
+++ src/abb.c
@@ -2,53 +2,372 @@
 #include "abb_estructura_privada.h"
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#define VACIO 0
 
 abb_t *abb_crear(abb_comparador comparador)
 {
-	return NULL;
+	if (!comparador)
+		return NULL;
+
+	abb_t *arbol = malloc(sizeof(abb_t));
+
+	if (!arbol)
+		return NULL;
+
+	arbol->comparador = comparador;
+	arbol->nodo_raiz = NULL;
+	arbol->tamanio = 0;
+
+	return arbol;
+}
+
+nodo_abb_t *crear_nodo(void *elemento)
+{
+	nodo_abb_t *nodo = malloc(sizeof(nodo_abb_t));
+
+	if (!nodo)
+		return NULL;
+	nodo->derecha = NULL;
+	nodo->izquierda = NULL;
+	nodo->elemento = elemento;
+
+	return nodo;
+}
+
+nodo_abb_t *abb_insertar_recursivo(nodo_abb_t *raiz, void *elemento,
+				   abb_comparador comparador)
+{
+	if (!raiz)
+		return crear_nodo(elemento);
+
+	int comparacion = comparador(elemento, raiz->elemento);
+
+	if (comparacion <= 0)
+		raiz->izquierda = abb_insertar_recursivo(raiz->izquierda,
+							 elemento, comparador);
+	else if (comparacion > 0) {
+		raiz->derecha = abb_insertar_recursivo(raiz->derecha, elemento,
+						       comparador);
+	}
+
+	return raiz;
 }
 
 abb_t *abb_insertar(abb_t *arbol, void *elemento)
 {
+	if (!arbol)
+		return NULL;
+
+	arbol->nodo_raiz = abb_insertar_recursivo(arbol->nodo_raiz, elemento,
+						  arbol->comparador);
+	arbol->tamanio++;
+
 	return arbol;
+}
+
+void *elemento_mas_derecho(nodo_abb_t *raiz, void **elemento)
+{
+	if (!raiz->derecha) {
+		*elemento = raiz->elemento;
+		nodo_abb_t *nodo_izquierdo = raiz->izquierda;
+		free(raiz);
+		return nodo_izquierdo;
+	}
+	raiz->derecha = elemento_mas_derecho(raiz->derecha, elemento);
+
+	return raiz;
+}
+
+void *abb_quitar_recursivo(nodo_abb_t *raiz, void *elemento,
+			   abb_comparador comparador, void **extraido)
+{
+	if (!raiz)
+		return NULL;
+
+	int comparacion = comparador(elemento, raiz->elemento);
+
+	if (comparacion == 0) {
+		nodo_abb_t *nodo_izquierdo = raiz->izquierda;
+		nodo_abb_t *nodo_derecho = raiz->derecha;
+		*extraido = raiz->elemento;
+
+		if (nodo_izquierdo && nodo_derecho) {
+			void *elemento_derecho = NULL;
+			raiz->izquierda = elemento_mas_derecho(
+				raiz->izquierda, &elemento_derecho);
+			raiz->elemento = elemento_derecho;
+			return raiz;
+		} else {
+			free(raiz);
+			if (!nodo_izquierdo)
+				return nodo_derecho;
+			return nodo_izquierdo;
+		}
+	} else if (comparacion < 0)
+		raiz->izquierda = abb_quitar_recursivo(
+			raiz->izquierda, elemento, comparador, extraido);
+	else
+		raiz->derecha = abb_quitar_recursivo(raiz->derecha, elemento,
+						     comparador, extraido);
+
+	return raiz;
 }
 
 void *abb_quitar(abb_t *arbol, void *elemento)
 {
-	return elemento;
+	if (!arbol || !arbol->nodo_raiz)
+		return NULL;
+	void *extraido = NULL;
+	arbol->nodo_raiz = abb_quitar_recursivo(arbol->nodo_raiz, elemento,
+						arbol->comparador, &extraido);
+
+	if (extraido != NULL)
+		arbol->tamanio--;
+
+	return extraido;
+}
+
+void *abb_buscar_recursivo(nodo_abb_t *raiz, void *elemento,
+			   abb_comparador comparador, void **elemento_aux)
+{
+	if (!raiz)
+		return NULL;
+
+	int comparacion;
+	elemento_aux = raiz->elemento;
+	comparacion = comparador(elemento, elemento_aux);
+
+	if (comparacion < 0)
+		elemento_aux = abb_buscar_recursivo(raiz->izquierda, elemento,
+						    comparador, elemento_aux);
+	else if (comparacion > 0)
+		elemento_aux = abb_buscar_recursivo(raiz->derecha, elemento,
+						    comparador, elemento_aux);
+
+	return elemento_aux;
 }
 
 void *abb_buscar(abb_t *arbol, void *elemento)
 {
-	return elemento;
+	if (!arbol || !arbol->nodo_raiz)
+		return NULL;
+	void **elemento_aux = abb_buscar_recursivo(
+		arbol->nodo_raiz, elemento, arbol->comparador, elemento_aux);
+	return elemento_aux;
 }
 
 bool abb_vacio(abb_t *arbol)
 {
-	return true;
+	if (!arbol || !arbol->nodo_raiz)
+		return true;
+	return false;
 }
 
 size_t abb_tamanio(abb_t *arbol)
 {
-	return 0;
+	if (!arbol)
+		return VACIO;
+	return arbol->tamanio;
+}
+
+void abb_destruir_recursivo(nodo_abb_t *raiz)
+{
+	if (!raiz)
+		return;
+
+	abb_destruir_recursivo(raiz->izquierda);
+	abb_destruir_recursivo(raiz->derecha);
+
+	free(raiz);
+	return;
 }
 
 void abb_destruir(abb_t *arbol)
 {
+	if (!arbol)
+		return;
+	abb_destruir_recursivo(arbol->nodo_raiz);
+	free(arbol);
+	return;
+}
+
+void abb_destuir_todo_recursivo(nodo_abb_t *raiz, void (*destructor)(void *))
+{
+	if (!raiz)
+		return;
+	abb_destuir_todo_recursivo(raiz->izquierda, destructor);
+	if (destructor != NULL)
+		destructor(raiz->elemento);
+
+	abb_destuir_todo_recursivo(raiz->derecha, destructor);
+
+	return;
 }
 
 void abb_destruir_todo(abb_t *arbol, void (*destructor)(void *))
 {
+	if (!arbol)
+		return;
+	abb_destuir_todo_recursivo(arbol->nodo_raiz, destructor);
+	abb_destruir(arbol);
+
+	return;
+}
+
+void abb_con_cada_elemento_postorden(nodo_abb_t *raiz,
+				     bool (*funcion)(void *, void *), void *aux,
+				     size_t *invocaciones, bool *continuar)
+{
+	if (!(*continuar) || !raiz)
+		return;
+
+	abb_con_cada_elemento_postorden(raiz->izquierda, funcion, aux,
+					invocaciones, continuar);
+	abb_con_cada_elemento_postorden(raiz->derecha, funcion, aux,
+					invocaciones, continuar);
+	if (!(*continuar))
+		return;
+	(*continuar) = funcion(raiz->elemento, aux);
+	(*invocaciones)++;
+	return;
+}
+
+void abb_con_cada_elemento_preorden(nodo_abb_t *raiz,
+				    bool (*funcion)(void *, void *), void *aux,
+				    size_t *invocaciones, bool *continuar)
+{
+	if (!(*continuar) || !raiz)
+		return;
+
+	(*continuar) = funcion(raiz->elemento, aux);
+	if (continuar)
+		(*invocaciones)++;
+	abb_con_cada_elemento_preorden(raiz->izquierda, funcion, aux,
+				       invocaciones, continuar);
+	abb_con_cada_elemento_preorden(raiz->derecha, funcion, aux,
+				       invocaciones, continuar);
+
+	return;
+}
+
+void abb_con_cada_elemento_inorden(nodo_abb_t *raiz,
+				   bool (*funcion)(void *, void *), void *aux,
+				   size_t *invocaciones, bool *continuar)
+{
+	if (!(*continuar) || !raiz)
+		return;
+
+	abb_con_cada_elemento_inorden(raiz->izquierda, funcion, aux,
+				      invocaciones, continuar);
+	if (!(*continuar))
+		return;
+	(*continuar) = funcion(raiz->elemento, aux);
+	(*invocaciones)++;
+	abb_con_cada_elemento_inorden(raiz->derecha, funcion, aux, invocaciones,
+				      continuar);
+
+	return;
 }
 
 size_t abb_con_cada_elemento(abb_t *arbol, abb_recorrido recorrido,
 			     bool (*funcion)(void *, void *), void *aux)
 {
-	return 0;
+	if (!arbol || !funcion)
+		return VACIO;
+
+	size_t cantidad_invocaciones = 0;
+	bool continuar = true;
+	if (recorrido == INORDEN)
+		abb_con_cada_elemento_inorden(arbol->nodo_raiz, funcion, aux,
+					      &cantidad_invocaciones,
+					      &continuar);
+	else if (recorrido == PREORDEN)
+		abb_con_cada_elemento_preorden(arbol->nodo_raiz, funcion, aux,
+					       &cantidad_invocaciones,
+					       &continuar);
+	else if (recorrido == POSTORDEN)
+		abb_con_cada_elemento_postorden(arbol->nodo_raiz, funcion, aux,
+						&cantidad_invocaciones,
+						&continuar);
+
+	return cantidad_invocaciones;
+}
+
+void abb_recorrer_postorden(nodo_abb_t *raiz, void **array,
+			    size_t tamanio_array, size_t *elementos_guardados)
+{
+	if (!raiz)
+		return;
+
+	abb_recorrer_postorden(raiz->izquierda, array, tamanio_array,
+			       elementos_guardados);
+	abb_recorrer_postorden(raiz->derecha, array, tamanio_array,
+			       elementos_guardados);
+
+	if ((*elementos_guardados) < tamanio_array) {
+		array[(*elementos_guardados)] = raiz->elemento;
+		(*elementos_guardados)++;
+	}
+
+	return;
+}
+
+void abb_recorrer_preorden(nodo_abb_t *raiz, void **array, size_t tamanio_array,
+			   size_t *elementos_guardados)
+{
+	if (!raiz)
+		return;
+
+	if ((*elementos_guardados) < tamanio_array) {
+		array[(*elementos_guardados)] = raiz->elemento;
+		(*elementos_guardados)++;
+	}
+
+	abb_recorrer_preorden(raiz->izquierda, array, tamanio_array,
+			      elementos_guardados);
+	abb_recorrer_preorden(raiz->derecha, array, tamanio_array,
+			      elementos_guardados);
+
+	return;
+}
+
+void abb_recorrer_inorden(nodo_abb_t *raiz, void **array, size_t tamanio_array,
+			  size_t *elementos_guardados)
+{
+	if (!raiz)
+		return;
+
+	abb_recorrer_inorden(raiz->izquierda, array, tamanio_array,
+			     elementos_guardados);
+	if ((*elementos_guardados) < tamanio_array) {
+		array[(*elementos_guardados)] = raiz->elemento;
+		(*elementos_guardados)++;
+	}
+	abb_recorrer_inorden(raiz->derecha, array, tamanio_array,
+			     elementos_guardados);
+	return;
 }
 
 size_t abb_recorrer(abb_t *arbol, abb_recorrido recorrido, void **array,
 		    size_t tamanio_array)
 {
-	return 0;
+	if (!arbol)
+		return VACIO;
+
+	size_t elementos_guardados = 0;
+
+	if (recorrido == INORDEN)
+		abb_recorrer_inorden(arbol->nodo_raiz, array, tamanio_array,
+				     &elementos_guardados);
+	else if (recorrido == PREORDEN)
+		abb_recorrer_preorden(arbol->nodo_raiz, array, tamanio_array,
+				      &elementos_guardados);
+	else if (recorrido == POSTORDEN)
+		abb_recorrer_postorden(arbol->nodo_raiz, array, tamanio_array,
+				       &elementos_guardados);
+
+	return elementos_guardados;
 }
